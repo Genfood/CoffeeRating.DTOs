32d2aba4446621aa93eb3c16d6b2880258a5eca0
Genfood <fhoerdt@protonmail.com>
2021-09-06 18:10:20 +0200

    add: token lifetime and add refresh token request

---
+++ src/V1/Requests/RefreshTokenRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeRating.DTOs.V1.Requests
+{
+    public class RefreshTokenRequest
+    {
+        public string RefreshToken { get; set; }
+    }
+}
