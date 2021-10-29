888af11db21f0bc038b62568cb78436b05575365
Genfood <fhoerdt@protonmail.com>
2021-10-29 14:12:26 +0200

    add Coffee roastery respone properties

--- src/V1/Responses/CoffeeRoasteryResponse.cs
+++ src/V1/Responses/CoffeeRoasteryResponse.cs
@@ -1,6 +1,12 @@
-﻿namespace CoffeeRating.DTOs.V1.Responses
+﻿using System.Collections.Generic;
+
+namespace CoffeeRating.DTOs.V1.Responses
 {
     public class CoffeeRoasteryResponse
     {
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public List<CoffeeResponse> Coffees { get; set; }
     }
 }
